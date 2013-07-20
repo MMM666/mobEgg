843d29d35bdfae6ddad8042e746873bcaabd8759
MMM666 <areast_666@hotmail.com>
2013-07-20 18:55:54 +0900

    1.6.2-2リリース。
    MMMLibに併せて更新。

--- mobEgg/net/minecraft/src/mod_IME_mobEgg.java
+++ mobEgg/net/minecraft/src/mod_IME_mobEgg.java
@@ -43,13 +43,13 @@ public class mod_IME_mobEgg extends BaseMod {
 
 	@Override
 	public String getVersion() {
-		return "1.6.2-1";
+		return "1.6.2-2";
 	}
 
 	@Override
 	public void load() {
 		// MMMLibのRevisionチェック
-		MMM_Helper.checkRevision("1");
+		MMM_Helper.checkRevision("3");
 		
 		entityMap = new TreeMap<String, Entity>();
 		
